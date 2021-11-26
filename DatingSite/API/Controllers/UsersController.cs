eb40fa21c1c3d0bb08c8d00fd77e47b1a8b04567
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-26 10:38:08 +0300

    Api bug fixed

--- DatingSite/API/Controllers/UsersController.cs
+++ DatingSite/API/Controllers/UsersController.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
-        
+        public readonly DataContext Context;
+        public UsersController(DataContext context)
+        {
+            this.Context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return Context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUsers(int id)
+        {
+            return Context.Users.Find(id);
+        }
     }
 }
