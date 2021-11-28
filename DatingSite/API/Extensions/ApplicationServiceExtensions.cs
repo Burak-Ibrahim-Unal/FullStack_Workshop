9e8b1fb0489f93ab30f7ea61ba4a7c4b43f77862
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-28 12:38:21 +0300

    Extensions added

---
+++ DatingSite/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using API.Interfaces;
+using API.Services;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+    }
+}
--- DatingSite/API/Program.cs
+++ DatingSite/API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -9,12 +10,15 @@ using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
+/* I added these lines to ApplicationServiceExtensionS */
+// builder.Services.AddScoped<ITokenService, TokenService>();
+// builder.Services.AddDbContext<DataContext>(options =>
+// {
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+// });
+
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddControllers();
 var myAngularPolicy = "myAngularPolicy";
 builder.Services.AddCors(options =>
@@ -23,20 +27,25 @@ builder.Services.AddCors(options =>
                           builder =>
                           {
                               builder.WithOrigins("https://localhost:4200")
-                                                  .AllowAnyHeader()
-                                                  .AllowAnyMethod();
+                                     .AllowAnyHeader()
+                                     .AllowAnyMethod();
                           });
 });
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+
+builder.Services.AddIdentityServices(builder.Configuration);    
+
+/* I added this lines to IdentityServiceExtensions */
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+// {
+//     options.TokenValidationParameters = new TokenValidationParameters
+//     {
+//         ValidateIssuerSigningKey = true,
+//         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+//         ValidateIssuer = false,
+//         ValidateAudience = false
+//     };
+// });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
