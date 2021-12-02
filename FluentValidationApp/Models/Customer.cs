7bd90848f05c63107b64a5766df69ddd71c178f6
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-02 18:01:10 +0300

    DateApp NetCore 5 started // Fluent Validation Test Started

---
+++ FluentValidationApp/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentValidationApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public int Age { get; set; }
+
+    }
+}
