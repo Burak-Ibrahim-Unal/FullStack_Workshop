ebba16f6ab57bca7715c5d16d95b5e5e0fa78d8c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-02 09:15:39 +0300

    New entity is added // AppUser entity is fixed // migration added

--- DatingSite/API/Entities/AppUser.cs
+++ DatingSite/API/Entities/AppUser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -11,5 +12,21 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalting { get; set; }
+        public DateTime Birthday { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Intro { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+
+        public int getAge()
+        {
+            return Birthday.CalculateAge();
+        }
     }
 }
