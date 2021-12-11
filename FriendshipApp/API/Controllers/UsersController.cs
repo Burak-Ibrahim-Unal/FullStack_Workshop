56bc3861bbaad1f31b020a9a7f4b65208de345b8
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-11 23:20:12 +0300

    user controller bug  fixed.

--- FriendshipApp/API/Controllers/UsersController.cs
+++ FriendshipApp/API/Controllers/UsersController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
         private readonly DataContext _datacontext;
