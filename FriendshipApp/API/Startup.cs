eb1d3d0d81d72b67a139714e9b226610a485097c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-12 18:38:31 +0300

    ssl added

--- FriendshipApp/API/Startup.cs
+++ FriendshipApp/API/Startup.cs
@@ -56,7 +56,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
