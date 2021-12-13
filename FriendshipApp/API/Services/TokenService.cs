ec2963f5986957213a08797ecf478f6831dd6064
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-13 18:24:25 +0300

    token creator is ready.

--- FriendshipApp/API/Services/TokenService.cs
+++ FriendshipApp/API/Services/TokenService.cs
@@ -1,9 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
 using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
-        
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokeyKey"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId,user.UserName)
+            };
+
+            var credentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credentials,
+
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            
+            var token=tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+
+
+        }
     }
 }
