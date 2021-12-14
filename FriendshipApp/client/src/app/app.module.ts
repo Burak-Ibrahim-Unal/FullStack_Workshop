731255fef6996f522e5078b847ddee2dc9aeb96b
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-14 21:28:09 +0300

    toastr module added

--- FriendshipApp/client/src/app/app.module.ts
+++ FriendshipApp/client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
@@ -34,6 +35,10 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserAnimationsModule,
     FormsModule,
     BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom",
+
+    }),
   ],
   providers: [
     {
