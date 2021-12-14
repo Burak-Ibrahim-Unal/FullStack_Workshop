cd39b39ff3564f610d996e73f558720dfa94716a
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-14 09:19:04 +0300

    BsDropdownModule is added to navbar dropdown

--- FriendshipApp/client/src/app/app.module.ts
+++ FriendshipApp/client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [
     {
