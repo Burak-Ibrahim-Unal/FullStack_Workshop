c2207c93e4ad461a0d3b9f0f737b91f33e99dee1
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-14 08:28:48 +0300

    navbar created

--- FriendshipApp/client/src/app/app.module.ts
+++ FriendshipApp/client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
