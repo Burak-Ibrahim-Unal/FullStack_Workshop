25e6352ba0b3849513a5e450c40e590a16c5a9d2
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-15 09:35:36 +0300

    routers are fixed, navbar fixed

--- FriendshipApp/client/src/app/app-routing.module.ts
+++ FriendshipApp/client/src/app/app-routing.module.ts
@@ -10,10 +10,18 @@ import { flush } from '@angular/core/testing';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "members", component: MemberListComponent, canActivate: [AuthGuard] },
-  { path: "members/:id", component: MemberDetailComponent },
-  { path: "lists", component: ListsComponent },
-  { path: "messages", component: MessagesComponent },
+  {
+    path: "",
+    runGuardsAndResolvers: "always",
+    canActivate: [AuthGuard],
+    children: [
+      { path: "members", component: MemberListComponent, canActivate: [AuthGuard] },
+      { path: "members/:id", component: MemberDetailComponent },
+      { path: "lists", component: ListsComponent },
+      { path: "messages", component: MessagesComponent },
+    ]
+  },
+
   { path: "**", component: HomeComponent, pathMatch: "full" },
 ];
 
