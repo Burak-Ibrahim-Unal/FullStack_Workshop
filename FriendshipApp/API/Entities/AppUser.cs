0ee756a3fefef6b846c055bc38d37b59dfa10970
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-17 14:33:53 +0300

    user entity updated // photo entity createad

--- FriendshipApp/API/Entities/AppUser.cs
+++ FriendshipApp/API/Entities/AppUser.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+
 namespace API.Entities
 {
     public class AppUser
@@ -6,5 +9,15 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime Birthday { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photos> Photos { get; set; }
     }
 }
---
+++ FriendshipApp/API/Entities/Photos.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class Photos
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+        
+    }
+}
