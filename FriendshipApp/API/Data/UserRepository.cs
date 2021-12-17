44684cdfd53d019c998d797ec0c800f30b34ca02
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-17 18:03:42 +0300

    Repository Design is started

---
+++ FriendshipApp/API/Data/UserRepository.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByNameAsync(string name)
+        {
+            return await _context.Users.SingleOrDefaultAsync(user => user.UserName == name);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State= EntityState.Modified;
+        }
+    }
+}
