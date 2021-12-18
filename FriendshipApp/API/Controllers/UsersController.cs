bdca8135c2542786559e922e2191f31a7d0f5732
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-18 10:23:33 +0300

    appuser info automapped memberdto

--- FriendshipApp/API/Controllers/UsersController.cs
+++ FriendshipApp/API/Controllers/UsersController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
+using System.Collections;
 
 namespace API.Controllers
 {
@@ -15,30 +18,37 @@ namespace API.Controllers
     {
 
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _userRepository = userRepository;
 
         }
 
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return Ok(await _userRepository.GetUsersAsync());
+            var users = await _userRepository.GetUsersAsync();
+
+            var returnUsers = _mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(returnUsers);
         }
 
         [HttpGet("{username}")] // api/users/1
-        public async Task<ActionResult<AppUser>> GetUserByName(string username)
+        public async Task<ActionResult<MemberDto>> GetUserByName(string username)
         {
-            return await _userRepository.GetUserByNameAsync(username);
+            var user = await _userRepository.GetUserByNameAsync(username);
+            return _mapper.Map<MemberDto>(user);
         }
 
         [HttpGet("{id:int}")] // api/users/1
-        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        public async Task<ActionResult<MemberDto>> GetUserById(int id)
         {
-            return await _userRepository.GetUserByIdAsync(id);
+            var user = await _userRepository.GetUserByIdAsync(id);
+            return _mapper.Map<MemberDto>(user);
 
         }
     }
--- FriendshipApp/API/Helpers/AutoMapperProfiles.cs
+++ FriendshipApp/API/Helpers/AutoMapperProfiles.cs
@@ -1,7 +1,15 @@
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+
 namespace API.Helpers
 {
-    public class AutoMapperProfiles
+    public class AutoMapperProfiles : Profile
     {
-        
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser,MemberDto>();
+            CreateMap<Photos,PhotoDto>();
+        }
     }
 }
