d40c10eb128a30611d926ae771cb3e792e30e409
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-22 08:56:54 +0300

    Add photo process started

--- FriendshipApp/client/src/app/_modules/shared.module.ts
+++ FriendshipApp/client/src/app/_modules/shared.module.ts
@@ -4,6 +4,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
 
 
 
@@ -18,13 +19,15 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
     }),
     TabsModule.forRoot(),
     NgxGalleryModule,
+    FileUploadModule,
 
   ],
-  exports:[
+  exports: [
     BsDropdownModule,
     ToastrModule,
     TabsModule,
     NgxGalleryModule,
+    FileUploadModule,
   ]
 })
 export class SharedModule { }
--- FriendshipApp/client/src/app/members/photo-editor/photo-editor.component.ts
+++ FriendshipApp/client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,10 @@
+import { AccountService } from './../../_services/account.service';
+import { environment } from './../../../environments/environment';
 import { Member } from 'src/app/_models/member';
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { User } from 'src/app/_models/user';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-editor',
@@ -8,12 +13,48 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() member: Member;
+  uploader: FileUploader;
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: User;
 
-
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  }
 
   ngOnInit(): void {
+    this.initializeUplaoder();
+  }
 
+
+  fileOverBase(newEvent: any) {
+    this.hasBaseDropzoneOver = newEvent; //event
+  }
+
+  initializeUplaoder() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + "users/add-photo",
+      authToken: "Bearer " + this.user.token,
+      isHTML5: true,
+      allowedFileType: ["image"],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024, //10mb
+
+    });
+
+    this.uploader.onAfterAddingAll = (file) => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    }
   }
 
 }
