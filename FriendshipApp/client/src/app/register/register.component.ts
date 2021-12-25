4e769817aef2e935fc67ee0ab4589468de0c7a05
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-25 23:44:55 +0300

    client side register completed // photo-edit bugs fixed // validation bug fixed // navbar fixed // date bug fixed

--- FriendshipApp/client/src/app/register/register.component.ts
+++ FriendshipApp/client/src/app/register/register.component.ts
@@ -1,7 +1,8 @@
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from './../_services/account.service';
 import { User } from './../_models/user';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
@@ -9,18 +10,26 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Vali
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, AfterViewInit {
   // @Input() usersFromHome: any;
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup;
   maxDate: Date;
+  validationErrors: string[] = [];
 
   constructor(
     private accountService: AccountService,
     private toastrService: ToastrService,
     private formBuilder: FormBuilder,
+    private router: Router,
+    private changeDetectorRef: ChangeDetectorRef,
   ) { }
+
+
+  ngAfterViewInit(): void {
+
+    this.changeDetectorRef.detectChanges();
+  }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -35,7 +44,7 @@ export class RegisterComponent implements OnInit {
       confirmPassword: ["", [Validators.required, this.matchPasswords("password")]],
       gender: ["male"],
       knownAs: ["", Validators.required],
-      birthday: ["", Validators.required],
+      birthday: [new Date(2000, 0, 1), Validators.required],
       city: ["", Validators.required],
       country: ["", Validators.required],
     });
@@ -56,16 +65,12 @@ export class RegisterComponent implements OnInit {
 
 
   register() {
-    console.log(this.registerForm.value);
-    // Old Way
-    //   this.accountService.register(this.model).subscribe(repsonse => {
-    //     this.toastrService.success("Register is success " + this.model.username, "Register Successful");
-    //     console.log(repsonse);
-    //     this.cancel();
-    //   }, error => {
-    //     this.toastrService.error(error.error + this.model.username, "Register Failed");
-    //     console.log(error);
-    //   });
+    this.accountService.register(this.registerForm.value).subscribe(repsonse => {
+      this.router.navigateByUrl("/members");
+      this.toastrService.success("Register is success ", "Successful");
+    }, error => {
+      this.validationErrors = error;
+    });
   }
 
   cancel() {
