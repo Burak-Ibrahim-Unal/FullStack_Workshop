b4a53b0293bdcefaa1965fe3a8147308ab76cf72
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-28 16:29:10 +0300

    Action filter is added

---
+++ FriendshipApp/API/Helpers/LogUserActivity.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var contextResult = await next();
+
+            if (!contextResult.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var username = contextResult.HttpContext.User.GetUsername();
+            var repo = contextResult.HttpContext.RequestServices.GetService<IUserRepository>();
+            var user = await repo.GetUserByNameAsync(username);
+            await repo.SaveAllAsync();
+
+        }
+    }
+}
