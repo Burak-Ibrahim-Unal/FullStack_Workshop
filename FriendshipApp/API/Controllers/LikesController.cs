366c946e267f032812a67405f19f742087b35653
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-01 15:40:42 +0300

    Update LikesController.cs

--- FriendshipApp/API/Controllers/LikesController.cs
+++ FriendshipApp/API/Controllers/LikesController.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
+using API.Extensions;
 using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class LikeController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
@@ -10,6 +15,19 @@ namespace API.Controllers
         {
             _likeRepository = likeRepository;
             _userRepository = userRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username)
+        {
+            var sourceUserId = User.GetUserId();
+            var likedUser = await _userRepository.GetUserByNameAsync(username);
+            var sourceUser = await _likeRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) return NotFound();
+            if (sourceUser.UserName == username) return BadRequest("You cant like yourself... :)");
+
+
         }
     }
 }
