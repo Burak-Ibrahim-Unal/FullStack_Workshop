3ad1ad9fd77092d52c893d63b3eab98590d9bfba
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-03 21:46:45 +0300

    Preperation for message controller

---
+++ FriendshipApp/API/DTOs/CreateMessage.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class CreateMessage
+    {
+        public string RecipientUsername { get; set; }
+        public string Content { get; set; }
+    }
+}
--- FriendshipApp/API/Helpers/AutoMapperProfiles.cs
+++ FriendshipApp/API/Helpers/AutoMapperProfiles.cs
@@ -12,13 +12,18 @@ namespace API.Helpers
         {
             CreateMap<AppUser, MemberDto>()
             .ForMember(destination => destination.PhotoUrl,
-                       option => option.MapFrom(target => target.Photos.FirstOrDefault(photo => photo.IsMain).Url))
+                       option => option.MapFrom(source => source.Photos.FirstOrDefault(photo => photo.IsMain).Url))
             .ForMember(destination => destination.Age,
                         target => target.MapFrom(source => source.Birthday.CalculateAge()));
             CreateMap<Photos, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
             CreateMap<RegisterDto, AppUser>();
-            
+            CreateMap<Message, MessageDto>()
+            .ForMember(destination => destination.SenderPhotoUrl,
+                        option => option.MapFrom(source => source.Sender.Photos.FirstOrDefault(p => p.IsMain).Url))
+            .ForMember(destination => destination.RecipientPhotoUrl,
+                        option => option.MapFrom(source => source.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url));
+
         }
     }
 }
