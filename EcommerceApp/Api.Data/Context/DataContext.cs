a9404724336010e86d0dbe9ccd2a731f6c03563d
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-09 22:36:59 +0300

    Readme created

--- EcommerceApp/Api.Data/Context/DataContext.cs
+++ EcommerceApp/Api.Data/Context/DataContext.cs
@@ -8,12 +8,12 @@ namespace API.Data.Context
 
         public DataContext(DbContextOptions options) : base(options)
         {
-
+            
         }
 
         public DataContext()
         {
-            
+
         }
 
         public DbSet<Product> Products { get; set; }
