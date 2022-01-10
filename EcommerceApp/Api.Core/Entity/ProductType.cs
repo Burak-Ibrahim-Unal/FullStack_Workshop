34ca73e5f90c3ffd75372a650b9f2339740ed22e
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-10 20:53:44 +0300

    New Entities are added

---
+++ EcommerceApp/Api.Core/Entity/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Core.Entity
+{
+    public class ProductType : BaseEntity
+    {
+        public string Name { get; set; }
+
+    }
+}
