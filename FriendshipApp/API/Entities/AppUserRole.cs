6f20d0a3951edfc739588b8743307aa5ca24cca9
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-16 12:25:36 +0300

    Update AppUserRole.cs

--- FriendshipApp/API/Entities/AppUserRole.cs
+++ FriendshipApp/API/Entities/AppUserRole.cs
@@ -5,8 +5,11 @@ namespace Api.Entities
 {
     public class AppUserRole : IdentityRole<int>
     {
+
         public AppUser User { get; set; }
+        public int UserId { get; set; }
         public AppRole Role { get; set; }
+        public int RoleId { get; set; }
 
     }
 }
