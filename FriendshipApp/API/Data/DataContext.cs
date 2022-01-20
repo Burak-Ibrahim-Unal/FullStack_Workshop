e011fdcd8d4f81983f3b8a4d0422835da98b706b
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-20 11:37:38 +0300

    Update DataContext.cs

--- FriendshipApp/API/Data/DataContext.cs
+++ FriendshipApp/API/Data/DataContext.cs
@@ -6,8 +6,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace API.Data
 {
-    public class DataContext : IdentityDbContext<AppUser, AppRole, int, IdentityUserClaim<int>, AppUserRole,
-        IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+    public class DataContext : IdentityDbContext<AppUser,AppRole,int,IdentityUserClaim<int> , 
+    AppUserRole,IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int> >
     {
         public DataContext(DbContextOptions options) : base(options)
         {
