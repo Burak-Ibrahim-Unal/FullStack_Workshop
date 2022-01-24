ea90183ff7819354c9d63482fc07915a609b2344
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-24 10:03:32 +0300

    Activities controller added // base api bug fixed

---
+++ ActivityApp/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities(){
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityById(Guid id){
+            return await _context.Activities.FindAsync(id);
+        }
+
+
+
+
+    }
+}
