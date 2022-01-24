2124d842b2d36c20f721355c8104e87ed8a775e6
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-24 14:15:02 +0300

    axios installed // simple activity list started

--- ActivityApp/client/src/App.tsx
+++ ActivityApp/client/src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://localhost:5001/api/activities").then(response => {
+      setActivities(response.data);
+    })
+  }, []);
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((activity: any) => {
+            <li key={activity.id}> {activity.title}</li>
+          })}
+        </ul>
       </header>
     </div>
   );
