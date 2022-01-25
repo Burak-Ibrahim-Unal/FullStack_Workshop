cc5bea508ccc7c9b111278f3e7bb2c33dcb0d55c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-25 17:22:02 +0300

    Create Activity Codes are prepared

--- ActivityApp/API/Controllers/ActivitiesController.cs
+++ ActivityApp/API/Controllers/ActivitiesController.cs
@@ -11,16 +11,23 @@ namespace API.Controllers
     {
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities(){
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
             return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivityById(Guid id){
-            return await Mediator.Send(new Details.Query{Id=id});
+        public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
 
+        [HttpPost]
+        public async Task<ActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
 
 
     }
