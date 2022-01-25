ac45a212ddbf17b985afda4a7802b0659e7a9c67
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-25 13:15:17 +0300

    Controllers updated

--- ActivityApp/API/Controllers/ActivitiesController.cs
+++ ActivityApp/API/Controllers/ActivitiesController.cs
@@ -1,30 +1,18 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-            
-
-        }
-
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
--- ActivityApp/API/Controllers/BaseApiController.cs
+++ ActivityApp/API/Controllers/BaseApiController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -10,8 +12,9 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
+        private Mediator _mediator;
 
-
-
+         protected IMediator Mediator => _mediator ?? HttpContext.RequestServices.GetService<IMediator>();
+        
     }
 }
