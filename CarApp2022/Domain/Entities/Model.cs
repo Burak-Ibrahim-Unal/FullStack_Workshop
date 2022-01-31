56e22aade8e7401dc3152eac82e112440f60868b
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-01-31 15:41:56 +0300

    new entities added

---
+++ CarApp2022/Domain/Entities/Model.cs
@@ -0,0 +1,39 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Model : Entity
+    {
+        public Model()
+        {
+
+        }
+
+        public Model(int id, string name, double dailyPrice, int transmissionTypeId, int fuelTypeId, int brandId, string imageUrl)
+        {
+            Id = id;
+            Name = name;
+            DailyPrice = dailyPrice;
+            TransmissionTypeId = transmissionTypeId;
+            FuelTypeId = fuelTypeId;
+            BrandId = brandId;
+            ImageUrl = imageUrl;
+        }
+
+
+        public string Name { get; set; }
+        public double DailyPrice { get; set; }
+        public int TransmissionTypeId { get; set; }
+        public int FuelTypeId { get; set; }
+        public int BrandId { get; set; }
+        public string ImageUrl { get; set; }
+
+        public virtual Brand Brand { get; set; }
+
+    }
+}
