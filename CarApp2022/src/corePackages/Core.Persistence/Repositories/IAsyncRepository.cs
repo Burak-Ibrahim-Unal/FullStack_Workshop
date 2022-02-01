b64e3e9a3ef9273e4d86f6f01b1cc872ca13c8bc
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-01 11:33:31 +0300

    PAgination and asyncrepos are added

--- CarApp2022/src/corePackages/Core.Persistence/Repositories/IAsyncRepository.cs
+++ CarApp2022/src/corePackages/Core.Persistence/Repositories/IAsyncRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Query;
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,19 +7,26 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application.Services.Repositories
+namespace Core.Persistence.Repositories
 {
     public interface IAsyncRepository<T> where T : class
     {
         // linq,predicate,expression,func...
         Task<T> GetAsync(Expression<Func<T, bool>> predicate);
-        Task<T> GetListAsync(Expression<Func<T, bool>> predicate = null,
+        Task<IPaginate<T>> GetListAsync(Expression<Func<T, bool>> predicate = null,
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
             Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
             int index = 0,
             int size = 10,
             bool enableTracking = true,
             CancellationToken cancellationToken = default);
+
+
+        IQueryable<T> Query();
+
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
 
 
 
