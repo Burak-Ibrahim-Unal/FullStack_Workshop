f3fd6d5926260b7094cdb1bc4bc12080443af752
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-02 16:03:47 +0300

    New dto and model added // basepageable model added

--- CarApp2022/src/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
+++ CarApp2022/src/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Features.Brands.Commands.CreateBrand;
+using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Models;
 using Application.Features.Models.Commands.CreateModel;
 using AutoMapper;
+using Core.Persistence.Paging;
 using Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -15,6 +18,9 @@ namespace Application.Features.Brands.Profiles
         public MappingProfiles()
         {
             CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+            CreateMap<Brand, BrandListDto>().ReverseMap();
+            CreateMap<IPaginate<Brand>, BrandListModel>().ReverseMap();
+
 
         }
 
