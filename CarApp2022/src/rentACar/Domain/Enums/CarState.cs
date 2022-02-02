51a130536f90f96cbf4404b3e5ab606ca72c225c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-02 15:37:44 +0300

    connection string fixed // migration is added // services implemented

---
+++ CarApp2022/src/rentACar/Domain/Enums/CarState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum CarState
+    {
+        Available = 1,
+        Rented = 2,
+        Maintance = 3,
+          
+    }
+}
--- CarApp2022/src/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ CarApp2022/src/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -53,6 +54,7 @@ namespace Persistence.Contexts
                 brand.ToTable("Brands").HasKey(b => b.Id);
                 brand.Property(p => p.Id).HasColumnName("Id");
                 brand.Property(p => p.Name).HasColumnName("Name");
+
                 brand.HasMany(p => p.Models);
 
 
@@ -62,6 +64,7 @@ namespace Persistence.Contexts
             {
                 model.ToTable("Models").HasKey(m => m.Id);
                 model.Property(p => p.Id).HasColumnName("Id");
+                model.Property(p => p.Name).HasColumnName("Name");
                 model.Property(p => p.DailyPrice).HasColumnName("DailyPrice");
                 model.Property(p => p.BrandId).HasColumnName("BrandId");
                 model.Property(p => p.TransmissionId).HasColumnName("TransmissionId");
@@ -73,6 +76,7 @@ namespace Persistence.Contexts
                 model.HasOne(p => p.Fuel);
                 model.HasMany(p => p.Cars);
 
+
             });
 
             modelBuilder.Entity<Color>(color =>
@@ -80,7 +84,8 @@ namespace Persistence.Contexts
                 color.ToTable("Colors").HasKey(c => c.Id);
                 color.Property(p => p.Id).HasColumnName("Id");
                 color.Property(p => p.Name).HasColumnName("Name");
-                color.HasMany(p => p.Models);
+
+                color.HasMany(p => p.Cars);
 
 
             });
@@ -90,6 +95,7 @@ namespace Persistence.Contexts
                 fuel.ToTable("Fuels").HasKey(f => f.Id);
                 fuel.Property(p => p.Id).HasColumnName("Id");
                 fuel.Property(p => p.Name).HasColumnName("Name");
+
                 fuel.HasMany(p => p.Models);
 
 
@@ -100,6 +106,7 @@ namespace Persistence.Contexts
                 transmission.ToTable("Transmissions").HasKey(t => t.Id);
                 transmission.Property(p => p.Id).HasColumnName("Id");
                 transmission.Property(p => p.Name).HasColumnName("Name");
+
                 transmission.HasMany(p => p.Models);
 
 
@@ -113,11 +120,42 @@ namespace Persistence.Contexts
                 car.Property(p => p.Plate).HasColumnName("Plate");
                 car.Property(p => p.ColorId).HasColumnName("ColorId");
                 car.Property(p => p.ModelId).HasColumnName("ModelId");
+                car.Property(p => p.CarState).HasColumnName("State");
 
                 car.HasOne(p => p.Color);
                 car.HasOne(p => p.Model);
 
             });
+
+
+            // Seed Data
+            var brand1 = new Brand(1, "renault");
+            var brand2 = new Brand(2, "honda");
+            modelBuilder.Entity<Brand>().HasData(brand1, brand2);
+
+            var color1 = new Color(1, "red");
+            var color2 = new Color(2, "black");
+            modelBuilder.Entity<Color>().HasData(color1, color2);
+
+            var transmission1 = new Transmission(1, "manuel");
+            var transmission2 = new Transmission(2, "auto");
+            modelBuilder.Entity<Transmission>().HasData(transmission1, transmission2);
+
+
+            var fuel1 = new Fuel(1, "diesel");
+            var fuel2 = new Fuel(2, "gasoline");
+            modelBuilder.Entity<Fuel>().HasData(fuel1, fuel2);
+
+            var model1 = new Model(1, "kangoo", 500, 1, 1, 1, "");
+            var model2 = new Model(2, "civic", 1000, 2, 2, 2, "");
+            modelBuilder.Entity<Model>().HasData(model1, model2);
+
+
+            modelBuilder.Entity<Car>().HasData(new Car(1, 1, 1, "06ABC06", 2012, CarState.Available));
+            modelBuilder.Entity<Car>().HasData(new Car(2, 2, 2, "01DEF01", 2015, CarState.Available));
+
+
+
 
 
         }
