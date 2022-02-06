a364d6d573a660b3cbc53bd7200be2c2bb8497df
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-06 20:53:04 +0300

    Messages added

---
+++ CarApp2022/src/corePackages/Core.Utilities/Messages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities
+{
+    public class Messages
+    {
+        //Brand Messages
+
+        public static string BrandNameExists = "Brand name already exists";
+
+        //Car Messages
+        public static string CarAdded = "Araç başarıyla eklendi";
+
+        //Color Messages
+
+        //Model Messages
+
+        //Transmission Messages
+
+        //Fuel Messages
+    }
+}
