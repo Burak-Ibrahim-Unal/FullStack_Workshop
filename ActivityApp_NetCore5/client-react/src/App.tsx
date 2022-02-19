ff38046132fd45d206f60b4564f55159903e6bf3
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-19 14:45:27 +0300

    semantic ui bugs fixed

--- ActivityApp_NetCore5/client-react/src/App.tsx
+++ ActivityApp_NetCore5/client-react/src/App.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:5001/api/activities").then(response => {
-      console.log(response);
-      console.log("-------------------");
       console.log(response.data);
       setActivities(response.data);
     });
@@ -18,16 +16,16 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {activities.map((activity: any) => (
-            <li key={activity.id}>
-              {activity.title}
-            </li>
-          ))}
-        </ul>
-      </header>
+      <Header as="h2" icon="users" content="Activities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+
+      </List>
+
     </div>
   );
 }
