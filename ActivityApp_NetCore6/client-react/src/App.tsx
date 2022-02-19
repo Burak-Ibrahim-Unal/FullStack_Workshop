e7dbf4c61fe76de97f9809c390ba2b3345f19015
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-19 14:50:55 +0300

    net  core 6 client

--- ActivityApp_NetCore6/client-react/src/App.tsx
+++ ActivityApp_NetCore6/client-react/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5001/api/activities").then(response => {
+      console.log(response.data);
+      setActivities(response.data);
+    });
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as="h2" icon="users" content="Activities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+
+      </List>
+
     </div>
   );
 }
