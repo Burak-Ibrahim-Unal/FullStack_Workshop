4d38796ef9d6142637935d9c6d0ff2cc40dea455
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-19 17:23:42 +0300

    create activity is ready

--- ActivityApp_NetCore5/ActivityApp/API/Controllers/ActivitiesController.cs
+++ ActivityApp_NetCore5/ActivityApp/API/Controllers/ActivitiesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Controllers;
+using Application.Features.Activities.Commands;
 using Application.Features.Activities.Queries;
 using Domain.Entities;
 using MediatR;
@@ -25,6 +26,14 @@ namespace API
         public async Task<ActionResult<Activity>> GetActivityById(Guid id)
         {
             return await Mediator.Send(new GetActivityByIdQuery.Query { Id = id });
+        }
+
+
+        // if we dont return anything and if we just need  http responses(return,ok,bad request,not found), we ca use use IActionResult
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new CreateActivityCommand.Command{ Activity = activity}));
         }
 
 
