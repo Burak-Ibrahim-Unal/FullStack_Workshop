ba3a271d81726847fd4535fb667898161bc913bd
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-19 12:51:09 +0300

    state,usestate,useffecton activity

--- ActivityApp_NetCore5/client-react/src/App.tsx
+++ ActivityApp_NetCore5/client-react/src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5001/api/activities").then(response => {
+      console.log(response);
+      console.log("-------------------");
+      console.log(response.data);
+      setActivities(response.data);
+    });
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
