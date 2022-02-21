199e51d5964ba441ab714e4134c30bc6e1cb681f
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-02-21 20:23:04 +0300

    Javascript Map function implemented

--- ActivityApp_NetCore5/client-react/src/app/stores/activityStore.ts
+++ ActivityApp_NetCore5/client-react/src/app/stores/activityStore.ts
@@ -4,25 +4,29 @@ import { Activity } from '../models/activity';
 import { v4 as uuid } from 'uuid';
 
 export default class ActivityStore {
-      activities: Activity[] = [];
+      activityRegistry = new Map<string, Activity>();
       selectedActivity: Activity | undefined = undefined;
       editMode = false;
       loading = false;
-      loadingInitial = false;
+      loadingInitial = true;
 
       constructor() {
             makeAutoObservable(this)
       }
 
 
+      get activitiesByDate() {
+            return Array.from(this.activityRegistry.values()).sort((a, b) =>
+                  Date.parse(a.date) - Date.parse(b.date));
+      }
+
       loadActivities = async () => {
-            this.setLoadingInitial(true);
             try {
 
                   const activities = await agent.Activities.list();
                   activities.forEach(activity => {
                         activity.date = activity.date.split("T")[0];
-                        this.activities.push(activity);
+                        this.activityRegistry.set(activity.id, activity);
                   })
                   this.setLoadingInitial(false);
 
@@ -39,7 +43,7 @@ export default class ActivityStore {
       }
 
       selectActivity = (id: string) => {
-            this.selectedActivity = this.activities.find(a => a.id === id);
+            this.selectedActivity = this.activityRegistry.get(id);
       }
 
       cancelSelectedActivity = () => {
@@ -61,7 +65,7 @@ export default class ActivityStore {
             try {
                   await agent.Activities.create(activity)
                   runInAction(() => {
-                        this.activities.push(activity);
+                        this.activityRegistry.set(activity.id, activity);
                         this.selectedActivity = activity;
                         this.editMode = false;
                         this.loading = false;
@@ -80,7 +84,7 @@ export default class ActivityStore {
             try {
                   await agent.Activities.update(activity);
                   runInAction(() => {
-                        this.activities = [...this.activities.filter(a => a.id !== activity.id)];
+                        this.activityRegistry.set(activity.id, activity);
                         // this.activities.push(activity);
                         this.selectedActivity = activity;
                         this.editMode = false;
@@ -99,7 +103,7 @@ export default class ActivityStore {
             try {
                   await agent.Activities.delete(id);
                   runInAction(() => {
-                        this.activities = [...this.activities.filter(a => a.id !== id)];
+                        this.activityRegistry.delete(id);
                         if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
                         this.loading = false;
                   })
--- ActivityApp_NetCore5/client-react/src/features/activity/dashboard/ActivityList.tsx
+++ ActivityApp_NetCore5/client-react/src/features/activity/dashboard/ActivityList.tsx
@@ -5,7 +5,7 @@ import { useStore } from '../../../app/stores/store';
 
 export default observer(function ActivityList() {
     const { activityStore } = useStore();
-    const { deleteActivity, activities, loading } = activityStore;
+    const { deleteActivity, activitiesByDate, loading } = activityStore;
 
     const [target, setTarget] = useState("");
 
@@ -17,7 +17,7 @@ export default observer(function ActivityList() {
     return (
         <Segment>
             <Item.Group divided>
-                {activities.map(activity => (
+                {activitiesByDate.map(activity => (
                     <Item key={activity.id}>
                         <Item.Content>
                             <Item.Header as="a">{activity.title}</Item.Header>
