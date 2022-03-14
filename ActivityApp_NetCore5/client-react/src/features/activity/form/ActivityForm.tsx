200560424bf6cfcc414666afbabf5b16acc488cd
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-03-14 14:08:29 +0300

    custominputs added

--- ActivityApp_NetCore5/client-react/src/features/activity/form/ActivityForm.tsx
+++ ActivityApp_NetCore5/client-react/src/features/activity/form/ActivityForm.tsx
@@ -1,11 +1,13 @@
 import { observer } from "mobx-react-lite";
 import React, { ChangeEvent, useEffect, useState } from "react";
-import { Button, Segment } from "semantic-ui-react";
+import { Button, FormCustomTextInput, Label, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { NavLink, useHistory, useParams } from 'react-router-dom';
 import LoadingComponent from "../../../app/layout/LoadingComponents";
 import { v4 as uuid } from 'uuid';
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import CustomTextInput from "../../../app/common/form/customTextInput";
 
 
 
@@ -14,6 +16,7 @@ export default observer(function ActivityForm() {
     const { activityStore } = useStore();
     const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore;
     const { id } = useParams<{ id: string }>();
+
 
 
     const [activity, setActivity] = useState({
@@ -25,6 +28,18 @@ export default observer(function ActivityForm() {
         city: "",
         venue: ""
     });
+
+
+    const validationSchema = Yup.object({
+        title: Yup.string().required("Activity title can not be null"),
+        description: Yup.string().required("Activity description can not be null"),
+        category: Yup.string().required("Activity category can not be null"),
+        date: Yup.string().required(),
+        city: Yup.string().required("Activity city can not be null"),
+        venue: Yup.string().required("Activity venue can not be null"),
+
+    });
+
 
     // explanation mark means it can be undefined
     useEffect(() => {
@@ -59,15 +74,20 @@ export default observer(function ActivityForm() {
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+            <Formik
+                enableReinitialize
+                initialValues={activity}
+                onSubmit={values => console.log(values)}
+                validationSchema={validationSchema}
+            >
                 {({ handleSubmit }) => (
                     <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-                        <Field placeholder="Title" name="title"  />
-                        <Field placeholder="Description" name="description"  />
-                        <Field placeholder="Category" name="category"  />
-                        <Field type="date" placeholder="Date"name="date"  />
-                        <Field placeholder="City" name="city"  />
-                        <Field placeholder="Venue" name="venue"  />
+                        <CustomTextInput name="title" placeholder="Title" />
+                        <CustomTextInput placeholder="Description" name="description" />
+                        <CustomTextInput placeholder="Category" name="category" />
+                        <CustomTextInput placeholder="Date" name="date" />
+                        <CustomTextInput placeholder="City" name="city" />
+                        <CustomTextInput placeholder="Venue" name="venue" />
                         <Button floated="right" positive type="submit" content="Submit" loading={loading} />
                         <Button as={NavLink} to="/activities" floated="right" type="button" content="Cancel" />
                     </Form>
