456ee1a13c19766b0b4a57ebeede8e7706452e74
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-03-20 14:34:47 +0300

    identitydbcontext implemented

--- ActivityApp_NetCore5/ActivityApp/Persistence/Contexts/DataContext.cs
+++ ActivityApp_NetCore5/ActivityApp/Persistence/Contexts/DataContext.cs
@@ -1,9 +1,10 @@
 using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Contexts
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
