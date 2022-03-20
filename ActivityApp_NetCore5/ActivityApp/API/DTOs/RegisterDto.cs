8ada211faeee78f23b355d2aa42d5646233bd404
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-03-20 15:55:08 +0300

    Dtos added

---
+++ ActivityApp_NetCore5/ActivityApp/API/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+    }
+}
