35705b13d81fb0519ed7a263bd2cebf8ec11778c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-03-21 16:26:17 +0300

    logout functin is ready // token is installed to localstorage

--- ActivityApp_NetCore5/client-react/src/app/stores/commonStore.ts
+++ ActivityApp_NetCore5/client-react/src/app/stores/commonStore.ts
@@ -4,6 +4,8 @@ import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
       error: ServerError | null = null;
+      token: string | null = null;
+      appLoaded = false;
 
       constructor() {
             makeAutoObservable(this);
@@ -12,6 +14,11 @@ export default class CommonStore {
       setServerError = (error: ServerError) => {
             this.error = error;
 
+      }
+
+      setToken = (token: string | null) => {
+            if (token) window.localStorage.setItem("jwt", token);
+            this.token = token;
       }
 
 }
--- ActivityApp_NetCore5/client-react/src/app/stores/userStore.ts
+++ ActivityApp_NetCore5/client-react/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
 import { User, UserFormValues } from '../models/user';
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 import agent from '../api/agent';
+import { store } from './store';
+import { history } from '../..';
 
 export default class UserStore {
       user: User | null = null;
@@ -16,10 +18,19 @@ export default class UserStore {
       login = async (creds: UserFormValues) => {
             try {
                   const user = await agent.Accounts.login(creds);
-                  console.log(user);
+                  store.commonStore.setToken(user.token);
+                  runInAction(() => this.user = user);
+                  history.push("/activities")
             } catch (error) {
                   throw error;
             }
+      }
+
+      logout = () => {
+            store.commonStore.setToken(null);
+            window.localStorage.removeItem("jwt");
+            this.user = null;
+            history.push("/");
       }
 
 }
