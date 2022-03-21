f9722b82f3d36b979291bea3d0f33c44601516b6
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-03-21 14:27:47 +0300

    Account methods created

--- ActivityApp_NetCore5/client-react/src/app/api/agent.ts
+++ ActivityApp_NetCore5/client-react/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity } from '../models/activity';
+import { User, UserFormValues } from '../models/user';
 import CommonStore from '../stores/commonStore';
 import { store } from "../stores/store";
 
@@ -80,8 +81,17 @@ const Activities = {
       delete: (id: string) => axios.delete<void>(`/activities/${id}`),
 
 }
+
+const Account = {
+      current: () => requests.get<User>("/account"),
+      login: (user: UserFormValues) => requests.post<User>("/account/login", user),
+      register: (user: UserFormValues) => requests.post<User>("/account/register", user),
+
+}
+
 const agent = {
-      Activities
+      Activities,
+      Account
 }
 
 export default agent;
---
+++ ActivityApp_NetCore5/client-react/src/app/models/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+      username: string;
+      displayName: string;
+      token: string;
+      image?: string;
+}
+
+
+export interface UserFormValues {
+      email: string;
+      password: string;
+      displayName?: string;
+      username?: string;
+
+}
