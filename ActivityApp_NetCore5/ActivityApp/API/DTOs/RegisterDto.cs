1d11f2ffbc7c1fbc79a33ba34c63a688aa3defb1
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-03-21 13:51:39 +0300

    Update RegisterDto.cs

--- ActivityApp_NetCore5/ActivityApp/API/DTOs/RegisterDto.cs
+++ ActivityApp_NetCore5/ActivityApp/API/DTOs/RegisterDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}")]
         public string Password { get; set; }
         public string Username { get; set; }
     }
