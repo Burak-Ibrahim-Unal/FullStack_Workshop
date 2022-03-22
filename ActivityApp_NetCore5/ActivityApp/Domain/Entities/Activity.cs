a5cbfc5e0782cb658af7b9fb968f1bdea24a068e
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-03-22 23:12:04 +0300

    Bug fixed

--- ActivityApp_NetCore5/ActivityApp/Domain/Entities/Activity.cs
+++ ActivityApp_NetCore5/ActivityApp/Domain/Entities/Activity.cs
@@ -12,7 +12,7 @@ namespace Domain.Entities
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
-        public ICollection<ActivityAttendee> Attendees { get; set; }
+        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
 
     }
 }
