dfffa006df06a510d7039b2bfc33cd02c6cf1986
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-05-10 17:04:57 +0300

    Photo update and delete prototypes and services are ready

--- ActivityApp_NetCore5/ActivityApp/API/Extensions/ApplicationServiceExtensions.cs
+++ ActivityApp_NetCore5/ActivityApp/API/Extensions/ApplicationServiceExtensions.cs
@@ -43,6 +43,7 @@ namespace API.Extensions
             services.AddMediatR(typeof(GetActivityListQuery.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor,PhotoAccessor>();
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
 
---
+++ ActivityApp_NetCore5/ActivityApp/Infrastructure/Photos/PhotoAccessor.cs
@@ -0,0 +1,7 @@
+namespace Infrastructure.Photos
+{
+    public class PhotoAccessor
+    {
+        
+    }
+}
