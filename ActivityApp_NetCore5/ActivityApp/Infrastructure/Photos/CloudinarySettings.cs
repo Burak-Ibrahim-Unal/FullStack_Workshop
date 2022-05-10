8539126fb40e6621cf2e329aba497e8adcf396e2
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-05-10 16:45:35 +0300

    Update CloudinarySettings.cs

--- ActivityApp_NetCore5/ActivityApp/Infrastructure/Photos/CloudinarySettings.cs
+++ ActivityApp_NetCore5/ActivityApp/Infrastructure/Photos/CloudinarySettings.cs
@@ -5,5 +5,6 @@ namespace Infrastructure.Photos
         public string CloudName { get; set; }
         public string ApiKey { get; set; }
         public string ApiSecret { get; set; }
+        
     }
 }
