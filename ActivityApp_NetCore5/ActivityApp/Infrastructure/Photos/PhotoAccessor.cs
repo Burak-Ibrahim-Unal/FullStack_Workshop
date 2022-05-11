6c92fd3d1e985eca0f22679d4120a122818a089b
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-05-11 08:51:56 +0300

    Bug fixed

--- ActivityApp_NetCore5/ActivityApp/Infrastructure/Photos/PhotoAccessor.cs
+++ ActivityApp_NetCore5/ActivityApp/Infrastructure/Photos/PhotoAccessor.cs
@@ -1,7 +1,20 @@
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Application.Photos;
+using Microsoft.AspNetCore.Http;
+
 namespace Infrastructure.Photos
 {
-    public class PhotoAccessor
+    public class PhotoAccessor : IPhotoAccessor
     {
-        
+        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> DeletePhoto(string publicId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
