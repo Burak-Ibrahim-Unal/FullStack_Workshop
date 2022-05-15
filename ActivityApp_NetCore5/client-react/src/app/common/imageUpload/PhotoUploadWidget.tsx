15c401e6e7e4c13916b1deafcfcfb9a96d9a8dc8
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-05-15 22:33:29 +0300

    react dropzone implemented

--- ActivityApp_NetCore5/client-react/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ ActivityApp_NetCore5/client-react/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Grid, Header } from "semantic-ui-react";
+import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
 
 export default function PhotoUploadWidget(){
     return(
         <Grid>
             <Grid.Column width={4}>
                 <Header sub color="teal" content="Add Photo"/>
+                <PhotoWidgetDropzone />
             </Grid.Column>
             <Grid.Column width={1}/>
             <Grid.Column width={4}>
