9969626404253412dab168b79afed6a9c77328ca
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-05-15 22:05:50 +0300

    add photo page is created

--- ActivityApp_NetCore5/client-react/src/features/profiles/ProfilePhotos.tsx
+++ ActivityApp_NetCore5/client-react/src/features/profiles/ProfilePhotos.tsx
@@ -1,23 +1,46 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
-import { Card, Header, Image, Tab } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Card, Header, Image, Tab, Grid, Button } from 'semantic-ui-react';
 import { Profile } from "../../app/models/profile";
+import { useStore } from '../../app/stores/store';
 
 interface Props {
     profile: Profile;
 }
 
 export default observer(function ProfilePhotos({ profile }: Props) {
+    const { profileStore: { isCurrentUser } } = useStore();
+    const [addPhotoMode, setAddPhotoMode] = useState(false);
+
     return (
         <Tab.Pane>
-            <Header icon="image" content="Photos" />
-            <Card.Group itemsPerRow={5}>
-                {profile.photos?.map(photo => (
-                    <Card key={photo.id}>
-                        <Image src={photo.url} />
-                    </Card>
-                ))}
-            </Card.Group>
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated="left" icon="image" content="Photos" />
+                    {isCurrentUser && (
+                        <Button
+                            floated="right"
+                            basic
+                            content={addPhotoMode ? "Cancel" : "AddPhoto"}
+                            onClick={() => setAddPhotoMode(!addPhotoMode)}
+                        />
+                    )}
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    {addPhotoMode ? (
+                        <p>Photo Mode Here</p>
+                    ) : (
+                        <Card.Group itemsPerRow={5}>
+                            {profile.photos?.map(photo => (
+                                <Card key={photo.id}>
+                                    <Image src={photo.url} />
+                                </Card>
+                            ))}
+                        </Card.Group>
+                    )}
+                </Grid.Column>
+            </Grid>
+
         </Tab.Pane>
     )
 })
