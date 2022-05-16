cb40332cb907d1f9fceedaf11e03af00ca5215f0
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-05-16 11:30:56 +0300

    cropper is added

--- ActivityApp_NetCore5/client-react/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ ActivityApp_NetCore5/client-react/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
 import { Grid, Header, Image } from "semantic-ui-react";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
+import { useEffect } from 'react';
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
+import { Cropper } from "react-cropper";
+import "cropperjs/dist/cropper.css";
+
 
 export default function PhotoUploadWidget() {
     const [files, setFiles] = useState<any>([]);
+    const [cropper, setCropper] = useState<Cropper>();
+
+    function onCrop() {
+        if (cropper) {
+            cropper.getCroppedCanvas().toBlob(blob => console.log(blob));
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            files.foreach((file: any) => URL.revokeObjectURL(file.preview))
+        }
+    }, [files])
 
     return (
         <Grid>
@@ -15,13 +33,17 @@ export default function PhotoUploadWidget() {
             <Grid.Column width={4}>
                 <Header sub color="teal" content="Resize" />
                 {files && files.length > 0 && (
-                    <Image src={files[0].preview} />
+                    // <Image src={files[0].preview} />
+                    <PhotoWidgetCropper setCropper={setCropper} imagePreview={files[0].preview} />
                 )}
             </Grid.Column>
             <Grid.Column width={1} />
             <Grid.Column width={4}>
                 <Header sub color="teal" content="Upload" />
+                <div className="img-preview" style={{ minHeight: 200, overflow: "hidden" }}>
+
+                </div>
             </Grid.Column>
-        </Grid>
+        </Grid >
     )
 }
