1bacc647f2b48fc7acc5717095bce09e147142d1
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-19 16:13:54 +0300

    react hook test

--- EcommerceApp/client/ecom/src/App.tsx
+++ EcommerceApp/client/ecom/src/App.tsx
@@ -1,20 +1,40 @@
-const products = [
+import { useEffect, useState } from "react";
+
+const productList = [
   { name: "product1", price: 3 },
   { name: "product2", price: 5 },
 ];
 
 function App() {
+  const [products, setProducts] = useState(productList);
+
+  function addProduct() {
+    setProducts((prevState) => [
+      ...prevState,
+      { name: "product" + (prevState.length + 1), price: prevState.length * 2 },
+    ]);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5029/api/Products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+
+    return () => {};
+  }, []);
+
   return (
     <div>
       <h1>Ecom</h1>
       <ul>
-        {products.map((item) => (
-          <li key={item.name}>
+        {products.map((item, index) => (
+          <li key={index}>
             {" "}
             {item.name} --- {item.price}TL
           </li>
         ))}
       </ul>
+      <button onClick={addProduct}>Kaydet</button>
     </div>
   );
 }
--- KUSYS-Demo/client/kusys/src/App.tsx
+++ KUSYS-Demo/client/kusys/src/App.tsx
@@ -1,17 +1,37 @@
-const students = [
+import { useEffect, useState } from "react";
+
+const studentList = [
   {name: "student1",birthday: "2000-11-15"},
   {name: "student2",birthday: "2000-1-5"},
 ]
 
 function App() {
+  const [students, setStudents] = useState(studentList);
+
+  function addStudent() {
+    setStudents((prevState) => [
+      ...prevState,
+      { name: "student" + (prevState.length + 1), birthday: "2001-01-01" },
+    ]);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5096/api/Students")
+      .then((response) => response.json())
+      .then((data) => setStudents(data))
+
+    return () => {};
+  }, []);
+
   return (
     <div >
       <h1 style={{border:'1px red solid'}}>Kusys</h1>
       <ul>
-        {students.map(s => (
-          <li key={s.name}>{s.name} --- {s.birthday} </li> 
+        {students.map((s,index) => (
+          <li key={index}>{s.name} --- {s.birthday} </li> 
         ))}
       </ul>
+      <button onClick={addStudent}>Kaydet</button>
     </div>
   );
 }
