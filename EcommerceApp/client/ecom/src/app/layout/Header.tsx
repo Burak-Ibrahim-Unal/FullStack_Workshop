05d3b5b5af35b987e2a31989d7879b5cce3716ef
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-20 23:04:20 +0300

    Router details

--- EcommerceApp/client/ecom/src/app/layout/Header.tsx
+++ EcommerceApp/client/ecom/src/app/layout/Header.tsx
@@ -1,8 +1,14 @@
+import { ShoppingCart } from "@mui/icons-material";
 import {
   AppBar,
+  Badge,
+  IconButton,
+  List,
+  ListItem,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { NavLink } from "react-router-dom";
 import DarkModeSwitch from "./DarkModeSwitch";
 
 interface Props {
@@ -10,12 +16,62 @@ interface Props {
   handleDarkThemeChange: () => void;
 }
 
+const headerMidLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+
+const headerRightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
+
 export default function Header({ darkMode, handleDarkThemeChange }: Props) {
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
       <Toolbar>
-        <Typography variant="h6">ECOM</Typography>
-        <DarkModeSwitch darkMode={darkMode} handleDarkThemeChange={handleDarkThemeChange}/>
+        <Typography
+          variant="h6"
+          component={NavLink}
+          to="/"
+          sx={{ color: "inherit", textDecoration: "none" }}
+        >
+          ECOM
+        </Typography>
+        <DarkModeSwitch
+          darkMode={darkMode}
+          handleDarkThemeChange={handleDarkThemeChange}
+        />
+        <List sx={{ display: "flex" }}>
+          {headerMidLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{ color: "inherit", typography: "h6" }}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <IconButton size="large" sx={{ color: "inherit" }}>
+          <Badge badgeContent={4} color="secondary">
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+        <List sx={{ display: "flex" }}>
+          {headerRightLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{ color: "inherit", typography: "h6" }}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
       </Toolbar>
     </AppBar>
   );
--- EcommerceApp/client/ecom/src/index.tsx
+++ EcommerceApp/client/ecom/src/index.tsx
@@ -1,14 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AboutPage from "./features/about/AboutPage";
-import Catalog from "./features/product/Catalog";
-import ProductDetail from "./features/product/ProductDetail";
-import ContactPage from "./features/contact/ContactPage";
-import HomePage from "./features/home/HomePage";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
