98abe19eb8a9cc141839c860bc3db57e00bd68be
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-23 00:49:03 +0300

    quantity add and remove functions are ready

--- EcommerceApp/client/ecom/src/features/basket/BasketPage.tsx
+++ EcommerceApp/client/ecom/src/features/basket/BasketPage.tsx
@@ -1,5 +1,7 @@
-import { Delete } from "@mui/icons-material";
+import { Add, Delete, Remove } from "@mui/icons-material";
+import { LoadingButton } from "@mui/lab";
 import {
+  Box,
   IconButton,
   Paper,
   Table,
@@ -10,10 +12,29 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
+import agent from "../../app/api/agent";
 import { useStoreContext } from "../../app/context/StoreContext";
 
 export default function BasketPage() {
-  const { basket } = useStoreContext();
+  const { basket, setBasket, removeItem } = useStoreContext();
+  const [loading, setLoading] = useState(false);
+
+  function handleAddItem(productId: number) {
+    setLoading(true);
+    agent.Basket.addItem(productId)
+      .then((basket) => setBasket(basket))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
+
+  function handleRemoveItem(productId: number, quantity = 1) {
+    setLoading(true);
+    agent.Basket.removeItem(productId, quantity)
+      .then(() => removeItem(productId, quantity))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
 
   if (!basket)
     return <Typography variant="h3">Your basket is empty...</Typography>;
@@ -25,7 +46,7 @@ export default function BasketPage() {
           <TableRow>
             <TableCell>Product</TableCell>
             <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="center">Quantity</TableCell>
             <TableCell align="right">Subtotal</TableCell>
             <TableCell align="right"></TableCell>
           </TableRow>
@@ -37,19 +58,46 @@ export default function BasketPage() {
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {item.name}
+                <Box display="flex" alignItems="center">
+                  <img
+                    src={item.pictureUrl}
+                    alt={item.name}
+                    style={{ height: 50, marginRight: 20 }}
+                  />
+                  <span>{item.name}</span>
+                </Box>
               </TableCell>
               <TableCell align="right">
                 ${(item.price / 100).toFixed(2)}
               </TableCell>
-              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="center">
+                <LoadingButton
+                  loading={loading}
+                  onClick={() => handleRemoveItem(item.productId)}
+                  color="error"
+                >
+                  <Remove />
+                </LoadingButton>
+                {item.quantity}
+                <LoadingButton
+                  loading={loading}
+                  onClick={() => handleAddItem(item.productId)}
+                  color="secondary"
+                >
+                  <Add />
+                </LoadingButton>
+              </TableCell>
               <TableCell align="right">
                 ${((item.price / 100) * item.quantity).toFixed(2)}
               </TableCell>
               <TableCell align="right">
-                <IconButton color="error">
+                <LoadingButton
+                  loading={loading}
+                  onClick={() =>handleRemoveItem(item.productId, item.quantity)}
+                  color="error"
+                >
                   <Delete />
-                </IconButton>
+                </LoadingButton>
               </TableCell>
             </TableRow>
           ))}
