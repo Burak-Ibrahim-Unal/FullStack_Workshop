32298e3624fb15f6b761eb4963ca6726f8e20027
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-23 01:18:23 +0300

    basket summary is ok

--- EcommerceApp/client/ecom/src/features/basket/BasketSummary.tsx
+++ EcommerceApp/client/ecom/src/features/basket/BasketSummary.tsx
@@ -1,34 +1,47 @@
-import { TableContainer, Paper, Table, TableBody, TableRow, TableCell, Typography } from "@mui/material";
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Typography,
+} from "@mui/material";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { currencyFormat } from "../../app/utilities/util";
 
 export default function BasketSummary() {
-    const subtotal = 0;
-    const deliveryFee = 0;
+  const { basket } = useStoreContext();
+  const subtotal = basket?.items.reduce((sum, item) => sum + item.quantity * item.price, 0) ?? 0;
+  const deliveryFee = subtotal > 10000 ? 0 : 500;
 
-    return (
-        <>
-            <TableContainer component={Paper} variant={'outlined'}>
-                <Table>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell colSpan={2}>Subtotal</TableCell>
-                            <TableCell align="right">{subtotal}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell colSpan={2}>Delivery fee*</TableCell>
-                            <TableCell align="right">{deliveryFee}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell colSpan={2}>Total</TableCell>
-                            <TableCell align="right">{subtotal + deliveryFee}</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>
-                                <span style={{fontStyle: 'italic'}}>*Orders over $100 qualify for free delivery</span>
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-    )
+  return (
+    <>
+      <TableContainer component={Paper} variant={"outlined"}>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell colSpan={2}>Subtotal</TableCell>
+              <TableCell align="right">{currencyFormat(subtotal)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}>Delivery fee*</TableCell>
+              <TableCell align="right">{currencyFormat(deliveryFee)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell colSpan={2}>Total</TableCell>
+              <TableCell align="right">{currencyFormat(subtotal + deliveryFee)}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <span style={{ fontStyle: "italic" }}>
+                  *Orders over $100 qualify for free delivery
+                </span>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
