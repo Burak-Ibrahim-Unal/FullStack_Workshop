bffbcbb6a050881ac43cc88a0cfe5a1a224c94ea
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-22 23:37:46 +0300

    basket table created

--- EcommerceApp/client/ecom/src/features/basket/BasketPage.tsx
+++ EcommerceApp/client/ecom/src/features/basket/BasketPage.tsx
@@ -1,4 +1,15 @@
-import { Typography } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
@@ -17,10 +28,46 @@ export default function BasketPage() {
 
   if (loading) return <LoadingComponent loadingMessage="Loading Basket..." />;
 
-//   if (basket)
-//     return <Typography variant="h3">Your basket is empty...</Typography>;
+  //   if (basket)
+  //     return <Typography variant="h3">Your basket is empty...</Typography>;
 
   return (
-     <h1>BuyerId = {basket!.buyerId}</h1>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket?.items.map((item) => (
+            <TableRow
+              key={item.productId}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {item.name}
+              </TableCell>
+              <TableCell align="right">
+                ${(item.price / 100).toFixed(2)}
+              </TableCell>
+              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="right">
+                ${((item.price / 100) * item.quantity).toFixed(2)}
+              </TableCell>
+              <TableCell align="right">
+                <IconButton color="error">
+                  <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
