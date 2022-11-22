35d1573785e23ba13cfec6efc25efb8c6f3a6b5e
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-22 11:18:18 +0300

    basket entity + controller + migratiion

---
+++ EcommerceApp/src/EcomApp/API/Controllers/BasketController.cs
@@ -0,0 +1,74 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly BaseDbContext _baseDbContext;
+
+        public BasketController(BaseDbContext baseDbContext)
+        {
+            _baseDbContext = baseDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await RetrieveBasket();
+
+            if (basket == null) return NotFound();
+
+            return basket;
+        }
+
+
+        [HttpPost] // api/basket?productId=3&quantity=2
+        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        {
+            var basket = await RetrieveBasket();
+            if (basket == null) basket = CreateBasket();
+
+            var product = await _baseDbContext.Products.FindAsync(productId);
+            if (product == null) return NotFound();
+
+            basket.AddItem(product, quantity);
+            var result = await _baseDbContext.SaveChangesAsync() > 0;
+
+            if(result) return StatusCode(201);
+            return BadRequest(new ProblemDetails { Title="Problem occured while saving basket"});
+        }
+
+
+        [HttpDelete]
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+        {
+            return StatusCode(201);
+
+        }
+
+
+        private async Task<Basket> RetrieveBasket()
+        {
+            return await _baseDbContext.Baskets
+                        .Include(i => i.Items)
+                        .ThenInclude(p => p.Product)
+                        .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+        }
+
+        private Basket? CreateBasket()
+        {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions { IsEssential = true, Expires = DateTime.Now.AddDays(30) };
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+
+            var basket = new Basket { BuyerId = buyerId };
+            _baseDbContext.Baskets.Add(basket);
+
+            return basket;
+        }
+    }
+}
--- EcommerceApp/src/EcomApp/Domain/Entities/BasketItem.cs
+++ EcommerceApp/src/EcomApp/Domain/Entities/BasketItem.cs
@@ -1,23 +1,29 @@
 ﻿using Core.Persistence.Repositories;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem : Entity
     {
         public int Quantity { get; set; }
         public int ProductId { get; set; }
+        public int BasketId { get; set; }
+
         public Product Product { get; set; }
+        public Basket Basket { get; set; }
 
         public BasketItem()
         {
 
         }
 
-        public BasketItem(int id,int quantity,int productId): base(id) 
+        public BasketItem(int id,int quantity,int productId,int basketID): base(id) 
         {
             Id = id;
             Quantity = quantity;
             ProductId = productId;
+            BasketId= basketID;
         }
     }
 }
