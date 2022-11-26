c334e95728003889e8202351d2d5d43b4e133222
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-27 01:09:11 +0300

    Entities and Ef repos are created

---
+++ KUSYS-v2/Domain/Entites/Course.cs
@@ -0,0 +1,15 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entites
+{
+    public class Course : Entity
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
