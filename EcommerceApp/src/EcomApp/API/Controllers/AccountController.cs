810bd3caeeb8f662ea2b3a52cf9e6bc1eef9b5c8
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-26 11:30:24 +0300

    account controller created

---
+++ EcommerceApp/src/EcomApp/API/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using API.DTOs;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.Username);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+                return Unauthorized();
+
+            return user;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            var user = new User { UserName = registerDto.Username, Email = registerDto.Email };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return ValidationProblem();
+            }
+
+            await _userManager.AddToRoleAsync(user, "Member");
+
+            return StatusCode(201);
+        }
+    }
---
+++ EcommerceApp/src/EcomApp/API/DTOs/RegisterDto.cs
@@ -0,0 +1,7 @@
+﻿namespace API.DTOs
+{
+    public class RegisterDto : LoginDto
+    {
+        public string Email { get; set; }
+    }
+}
