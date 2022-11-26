ddd7c07e580de6904600c396d4281bc680921a35
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-11-26 10:09:58 +0300

    identiy roles  created

--- EcommerceApp/src/EcomApp/Persistence/Contexts/BaseDbContext.cs
+++ EcommerceApp/src/EcomApp/Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Persistence.Contexts
 {
-    public class BaseDbContext : DbContext
+    public class BaseDbContext : IdentityDbContext<User>
     {
         public BaseDbContext(DbContextOptions options) : base(options)
         {
@@ -17,5 +19,16 @@ namespace Persistence.Contexts
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>()
+                .HasData(
+                    new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" },
+                    new IdentityRole { Name = "Member", NormalizedName = "MEMBER" }
+                );
+        }
     }
 }
