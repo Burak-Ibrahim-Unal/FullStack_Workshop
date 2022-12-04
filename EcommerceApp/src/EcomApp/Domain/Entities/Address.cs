9e5b746c3c99a6a4aab353fc9a771abb3760176f
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-12-04 16:12:31 +0300

    adress entity

---
+++ EcommerceApp/src/EcomApp/Domain/Entities/Address.cs
@@ -0,0 +1,20 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Address : Entity
+    {
+        public string FullName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+    }
+}
