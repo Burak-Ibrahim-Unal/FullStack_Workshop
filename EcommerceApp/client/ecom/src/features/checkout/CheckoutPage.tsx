180d426c05cff2ef8de45e9d8f72e3cdc5fe8e6c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-12-18 16:30:36 +0300

    checkout page  template is ready

--- EcommerceApp/client/ecom/src/features/checkout/CheckoutPage.tsx
+++ EcommerceApp/client/ecom/src/features/checkout/CheckoutPage.tsx
@@ -1,9 +1,81 @@
-import { Typography } from "@mui/material";
+import { Box, Button, Paper, Step, StepLabel, Stepper, Typography } from "@mui/material";
+import { useState } from "react";
+import AddressForm from "./AddressForm";
+import PaymentForm from "./PaymentForm";
+import Review from "./Review";
+
+const steps = ['Shipping address', 'Review your order', 'Payment details'];
+
+function getStepContent(step: number) {
+    switch (step) {
+        case 0:
+            return <AddressForm/>;
+        case 1:
+            return <Review/>;
+        case 2:
+            return <PaymentForm/>;
+        default:
+            throw new Error('Unknown step');
+    }
+}
 
 export default function CheckoutPage() {
+    const [activeStep, setActiveStep] = useState(0);
+
+    const handleNext = () => {
+        setActiveStep(activeStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep(activeStep - 1);
+    };
+
     return (
-        <Typography variant='h3'>
-            Only logged in users should be able to see this!
-        </Typography>
-    )
+
+        <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
+            <Typography component="h1" variant="h4" align="center">
+                Checkout
+            </Typography>
+            <Stepper activeStep={activeStep} sx={{pt: 3, pb: 5}}>
+                {steps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+            <>
+                {activeStep === steps.length ? (
+                    <>
+                        <Typography variant="h5" gutterBottom>
+                            Thank you for your order.
+                        </Typography>
+                        <Typography variant="subtitle1">
+                            Your order number is #2001539. We have emailed your order
+                            confirmation, and will send you an update when your order has
+                            shipped.
+                        </Typography>
+                    </>
+                ) : (
+                    <>
+                        {getStepContent(activeStep)}
+                        <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
+                            {activeStep !== 0 && (
+                                <Button onClick={handleBack} sx={{mt: 3, ml: 1}}>
+                                    Back
+                                </Button>
+                            )}
+                            <Button
+                                variant="contained"
+                                onClick={handleNext}
+                                sx={{mt: 3, ml: 1}}
+                            >
+                                {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                            </Button>
+                        </Box>
+                    </>
+                )}
+            </>
+        </Paper>
+    );
 }
+
