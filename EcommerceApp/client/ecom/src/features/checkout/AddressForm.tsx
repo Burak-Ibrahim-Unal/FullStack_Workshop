65c83a4ac87bc32fa2866656580316a8a591e975
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-12-20 14:43:29 +0300

    useform context is added

--- EcommerceApp/client/ecom/src/features/checkout/AddressForm.tsx
+++ EcommerceApp/client/ecom/src/features/checkout/AddressForm.tsx
@@ -1,53 +1,48 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import { useForm } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
-import { Button } from "@mui/material";
 
 export default function AddressForm() {
-  const { control, handleSubmit } = useForm();
+  const { control } = useFormContext();
   return (
     <>
       <Typography variant="h6" gutterBottom>
         Shipping address
       </Typography>
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={12}>
-            <AppTextInput control={control} name="fullName" label="Full Name" />
-          </Grid>
-          <Grid item xs={12}>
-            <AppTextInput control={control} name="address1" label="Address 1" />
-          </Grid>
-          <Grid item xs={12}>
-            <AppTextInput control={control} name="address2" label="Address 2" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <AppTextInput control={control} name="city" label="City" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <AppTextInput control={control} name="state" label="State" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <AppTextInput control={control} name="country" label="Country" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <AppTextInput control={control} name="zip" label="Zip" />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControlLabel
-              control={
-                <Checkbox color="secondary" name="saveAddress" value="yes" />
-              }
-              label="Use this address for payment details"
-            />
-          </Grid>
+       <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <AppTextInput control={control} name="fullName" label="Full Name" />
         </Grid>
-        <Button type="submit">Submit</Button>
-      </form>
+        <Grid item xs={12}>
+          <AppTextInput control={control} name="address1" label="Address 1" />
+        </Grid>
+        <Grid item xs={12}>
+          <AppTextInput control={control} name="address2" label="Address 2" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AppTextInput control={control} name="city" label="City" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AppTextInput control={control} name="state" label="State" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AppTextInput control={control} name="country" label="Country" />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <AppTextInput control={control} name="zip" label="Zip" />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={
+              <Checkbox color="secondary" name="saveAddress" value="yes" />
+            }
+            label="Use this address for payment details"
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }
--- EcommerceApp/client/ecom/src/features/checkout/CheckoutPage.tsx
+++ EcommerceApp/client/ecom/src/features/checkout/CheckoutPage.tsx
@@ -1,81 +1,93 @@
-import { Box, Button, Paper, Step, StepLabel, Stepper, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Paper,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
+import { FieldValues, FormProvider, useForm } from "react-hook-form";
 import AddressForm from "./AddressForm";
 import PaymentForm from "./PaymentForm";
 import Review from "./Review";
 
-const steps = ['Shipping address', 'Review your order', 'Payment details'];
+const steps = ["Shipping address", "Review your order", "Payment details"];
 
 function getStepContent(step: number) {
-    switch (step) {
-        case 0:
-            return <AddressForm/>;
-        case 1:
-            return <Review/>;
-        case 2:
-            return <PaymentForm/>;
-        default:
-            throw new Error('Unknown step');
-    }
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+      return <Review />;
+    case 2:
+      return <PaymentForm />;
+    default:
+      throw new Error("Unknown step");
+  }
 }
 
 export default function CheckoutPage() {
-    const [activeStep, setActiveStep] = useState(0);
+  const methods = useForm();
+  const [activeStep, setActiveStep] = useState(0);
 
-    const handleNext = () => {
-        setActiveStep(activeStep + 1);
-    };
+  const handleNext = (data: FieldValues) => {
+    if (activeStep === 0) {
+      console.log(data);
+    }
+    setActiveStep(activeStep + 1);
+  };
 
-    const handleBack = () => {
-        setActiveStep(activeStep - 1);
-    };
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
 
-    return (
-
-        <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
-            <Typography component="h1" variant="h4" align="center">
-                Checkout
-            </Typography>
-            <Stepper activeStep={activeStep} sx={{pt: 3, pb: 5}}>
-                {steps.map((label) => (
-                    <Step key={label}>
-                        <StepLabel>{label}</StepLabel>
-                    </Step>
-                ))}
-            </Stepper>
+  return (
+    <FormProvider {...methods}>
+      <Paper
+        variant="outlined"
+        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+      >
+        <Typography component="h1" variant="h4" align="center">
+          Checkout
+        </Typography>
+        <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <>
+          {activeStep === steps.length ? (
             <>
-                {activeStep === steps.length ? (
-                    <>
-                        <Typography variant="h5" gutterBottom>
-                            Thank you for your order.
-                        </Typography>
-                        <Typography variant="subtitle1">
-                            Your order number is #2001539. We have emailed your order
-                            confirmation, and will send you an update when your order has
-                            shipped.
-                        </Typography>
-                    </>
-                ) : (
-                    <>
-                        {getStepContent(activeStep)}
-                        <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
-                            {activeStep !== 0 && (
-                                <Button onClick={handleBack} sx={{mt: 3, ml: 1}}>
-                                    Back
-                                </Button>
-                            )}
-                            <Button
-                                variant="contained"
-                                onClick={handleNext}
-                                sx={{mt: 3, ml: 1}}
-                            >
-                                {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                            </Button>
-                        </Box>
-                    </>
-                )}
+              <Typography variant="h5" gutterBottom>
+                Thank you for your order.
+              </Typography>
+              <Typography variant="subtitle1">
+                Your order number is #2001539. We have emailed your order
+                confirmation, and will send you an update when your order has
+                shipped.
+              </Typography>
             </>
-        </Paper>
-    );
+          ) : (
+            <form onSubmit={methods.handleSubmit(handleNext)}>
+              {getStepContent(activeStep)}
+              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                {activeStep !== 0 && (
+                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                    Back
+                  </Button>
+                )}
+                <Button variant="contained" type="submit" sx={{ mt: 3, ml: 1 }}>
+                  {activeStep === steps.length - 1 ? "Place order" : "Next"}
+                </Button>
+              </Box>
+            </form>
+          )}
+        </>
+      </Paper>
+    </FormProvider>
+  );
 }
-
