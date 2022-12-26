2212ece925732a0b85b66d6800e7bcbfcf05a59e
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-12-26 19:19:51 +0300

    Order Page is ready

---
+++ EcommerceApp/client/ecom/src/features/orders/OrderPage.tsx
@@ -0,0 +1,66 @@
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { Order } from "../../app/models/order";
+import { currencyFormat } from "../../app/util/util";
+
+export default function OrderPage() {
+  const [orders, setOrders] = useState<Order[] | null>();
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    //setloading(true);
+    agent.Orders.list()
+      .then((orders) => setOrders(orders))
+      .catch((error) => console.log(error))
+      .finally(() => setloading(false));
+  }, []);
+
+  if (loading) return <LoadingComponent message="Loading Orders..." />;
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Order Number</TableCell>
+            <TableCell align="right">Total</TableCell>
+            <TableCell align="right">Order Date</TableCell>
+            <TableCell align="right">Order Status</TableCell>
+            <TableCell align="right"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {orders?.map((order) => (
+            <TableRow
+              key={order.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {order.id}
+              </TableCell>
+              <TableCell align="right">
+                {currencyFormat(order.totalCost)}
+              </TableCell>
+              <TableCell align="right">{order.orderDate.split("T")[0]}</TableCell>
+              <TableCell align="right">{order.orderStatus}</TableCell>
+              <TableCell align="right">
+                <Button>View</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
