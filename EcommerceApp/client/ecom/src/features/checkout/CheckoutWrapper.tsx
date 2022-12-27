228883f47d05e5beeb73f6a8c3661e2f4143f22c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-12-27 18:27:17 +0300

    submit payment function is ready

--- EcommerceApp/client/ecom/src/features/checkout/CheckoutWrapper.tsx
+++ EcommerceApp/client/ecom/src/features/checkout/CheckoutWrapper.tsx
@@ -1,5 +1,10 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { setBasket } from "../basket/basketSlice";
 import CheckoutPage from "./CheckoutPage";
 
 const stripePromise = loadStripe(
@@ -7,6 +12,18 @@ const stripePromise = loadStripe(
 );
 
 export default function CheckoutWrapper() {
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    agent.Payments.createPaymentIntent()
+      .then((basket) => dispatch(setBasket(basket)))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  if(loading) return <LoadingComponent message="Loading Checkout..."/>
+
   return (
     <Elements stripe={stripePromise}>
       <CheckoutPage />
